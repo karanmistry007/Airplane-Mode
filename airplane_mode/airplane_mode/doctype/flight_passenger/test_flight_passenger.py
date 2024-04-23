6e86f3eb774e23ab0c702a376c54b6a2ded8f378
karan-sanskar <karan@sanskartechnolab.com.com>
2024-04-24 00:11:34 +0530

    Day-1

---
+++ airplane_mode/airplane_mode/doctype/flight_passenger/test_flight_passenger.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Karan Mistry and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFlightPassenger(FrappeTestCase):
+	pass
